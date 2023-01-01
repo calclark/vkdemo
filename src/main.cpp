026c1041ae6670eb590de7b7d61c95c28f90777a
calclark <cadeclark16@gmail.com>
2022-12-31 20:23:35 -0500

    Include vulkan 1.3.238 + setup dynamic loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#define VULKAN_HPP_NO_EXCEPTIONS
+
 #include <fmt/core.h>
+
+#include <exception>
+#include <vulkan/vulkan.hpp>
 
 using fmt::print;
 
+// NOLINTNEXTLINE
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
 auto main() -> int
 {
+	vk::DynamicLoader loader;
+	auto vkGetInstanceProcAddr =
+			loader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
+	VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
+
+	auto [result, instance] = vk::createInstanceUnique({});
+	if (result != vk::Result::eSuccess) {
+		print(stderr, "Failed to create instance\n");
+	};
+	VULKAN_HPP_DEFAULT_DISPATCHER.init(*instance);
+
 	print("Hello, world!\n");
 	return 0;
 }
