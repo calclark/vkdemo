8d8963c6dbff58f0dc27109491e5dafb44b9a32f
calclark <cadeclark16@gmail.com>
2023-01-02 21:47:11 -0500

    Include glfw 3.3.8

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
+#define GLFW_INCLUDE_NONE
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #define VULKAN_HPP_NO_CONSTRUCTORS
 #define VULKAN_HPP_NO_EXCEPTIONS
 
 #include <fmt/core.h>
+#include <GLFW/glfw3.h>
 
 #include <exception>
 #include <utility>
@@ -29,8 +31,28 @@ auto check(vk::ResultValue<T> result, char const* message = "") -> T
 	return std::move(result.value);
 }
 
+auto glfw_error_callback(int error_code, char const* description) -> void
+{
+	print(stderr, "GLFW error {:#80X}: {}\n", error_code, description);
+}
+
 auto main() -> int
 {
+	glfwSetErrorCallback(glfw_error_callback);
+	if (glfwInit() == GLFW_FALSE) {
+		fail("Failed to initialize GLFW.");
+	}
+
+	if (glfwVulkanSupported() == GLFW_FALSE) {
+		fail("Vulkan is not supported by the installed graphics drivers.");
+	}
+
+	auto count = uint32_t{};
+	auto* t = glfwGetRequiredInstanceExtensions(&count);
+	for (auto i = uint32_t{0}; i < count; ++i) {
+		print("{}\n", t[i]);
+	}
+
 	auto loader = vk::DynamicLoader{};
 	auto vkGetInstanceProcAddr =
 			loader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
