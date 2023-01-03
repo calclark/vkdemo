7ae14979e2f38a562f12d35af5bc258a356fd1aa
calclark <cadeclark16@gmail.com>
2023-01-03 15:44:08 -0500

    Pick physical device

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <exception>
+#include <optional>
 #include <span>
 #include <utility>
 #include <vector>
@@ -20,6 +21,7 @@
 using fmt::print;
 using std::array;
 using std::move;
+using std::optional;
 using std::span;
 using std::terminate;
 using std::vector;
@@ -29,6 +31,8 @@ auto const window_height = 600;
 auto const application_name = "vkdemo";
 auto const validation_layers =
 		array<char const*, 1>{"VK_LAYER_KHRONOS_validation"};
+auto const device_extensions =
+		array<char const*, 1>{VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
 // NOLINTNEXTLINE
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
@@ -74,6 +78,26 @@ void glfw_key_callback(
 	}
 }
 
+class QueueFamilyIndices
+{
+ public:
+	optional<uint32_t> graphics_family;
+	optional<uint32_t> present_family;
+
+	[[nodiscard]] auto is_complete() const -> bool
+	{
+		return graphics_family.has_value() && present_family.has_value();
+	}
+};
+
+class SwapChainSupportDetails
+{
+ public:
+	vk::SurfaceCapabilitiesKHR capabilities;
+	vector<vk::SurfaceFormatKHR> formats;
+	vector<vk::PresentModeKHR> present_modes;
+};
+
 class Application
 {
  public:
@@ -93,6 +117,9 @@ class Application
 	vk::DynamicLoader _loader{};
 	vk::UniqueInstance _instance;
 	vk::UniqueSurfaceKHR _surface;
+	vk::PhysicalDevice _physical_device;
+	QueueFamilyIndices _queue_familes;
+	SwapChainSupportDetails _swapchain_details;
 
 	auto init_glfw() -> void
 	{
@@ -125,6 +152,7 @@ class Application
 		init_loader();
 		create_instance();
 		create_surface();
+		pick_physical_device();
 	}
 
 	auto init_loader() -> void
@@ -192,6 +220,104 @@ class Application
 				glfwCreateWindowSurface(_instance.get(), _window, nullptr, &surface),
 				"Failed to create a surface.");
 		_surface = vk::UniqueSurfaceKHR(surface, _instance.get());
+	}
+
+	auto pick_physical_device() -> void
+	{
+		auto devices = check(_instance->enumeratePhysicalDevices());
+		auto best = uint8_t{};
+		for (auto const& device : devices) {
+			auto queue_families = find_queue_families(device);
+			auto swapchain_details = swapchain_support(device);
+			auto score =
+					device_suitability(device, queue_families, swapchain_details);
+			if (score > best) {
+				_physical_device = device;
+				_queue_familes = queue_families;
+				_swapchain_details = swapchain_details;
+				best = score;
+			}
+		}
+		if (_physical_device == vk::PhysicalDevice{nullptr}) {
+			fail("Failed to find a suitable physical device.");
+		}
+	}
+
+	auto find_queue_families(vk::PhysicalDevice const& device)
+			-> QueueFamilyIndices
+	{
+		auto indices = QueueFamilyIndices{};
+		auto families = device.getQueueFamilyProperties();
+		auto idx = uint32_t{};
+		for (auto const& family : families) {
+			if (family.queueFlags & vk::QueueFlagBits::eGraphics) {
+				indices.graphics_family.emplace(idx);
+			}
+			auto present_support =
+					check(device.getSurfaceSupportKHR(idx, _surface.get()));
+			if (present_support == VK_TRUE) {
+				indices.present_family.emplace(idx);
+			}
+			if (indices.is_complete()) {
+				break;
+			}
+			idx += 1;
+		}
+		return indices;
+	}
+
+	auto swapchain_support(vk::PhysicalDevice const& device)
+			-> SwapChainSupportDetails
+	{
+		auto details = SwapChainSupportDetails{
+				check(device.getSurfaceCapabilitiesKHR(_surface.get())),
+				check(device.getSurfaceFormatsKHR(_surface.get())),
+				check(device.getSurfacePresentModesKHR(_surface.get())),
+		};
+		return details;
+	}
+
+	auto device_suitability(
+			vk::PhysicalDevice const& device,
+			QueueFamilyIndices const& queue_families,
+			SwapChainSupportDetails const& swapchain_details) -> uint8_t
+	{
+		if (!(queue_families.is_complete() && device_extensions_supported(device) &&
+					swapchain_adequate(swapchain_details))) {
+			return 0;
+		}
+		auto properties = device.getProperties();
+		switch (properties.deviceType) {
+			case vk::PhysicalDeviceType::eDiscreteGpu:
+				return 3;
+			case vk::PhysicalDeviceType::eIntegratedGpu:
+				return 2;
+			default:
+				return 1;
+		}
+	}
+
+	auto device_extensions_supported(vk::PhysicalDevice const& device) -> bool
+	{
+		auto available_extensions =
+				check(device.enumerateDeviceExtensionProperties());
+		for (auto const* required : device_extensions) {
+			auto found = false;
+			for (auto const& available : available_extensions) {
+				if (strcmp(available.extensionName, required) == 0) {
+					found = true;
+				}
+			}
+			if (!found) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	auto swapchain_adequate(SwapChainSupportDetails const& details) -> bool
+	{
+		return !(details.formats.empty() && details.present_modes.empty());
 	}
 
 	auto cleanup() -> void
